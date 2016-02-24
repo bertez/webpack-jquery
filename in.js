6eeebb6acfa1b3126fe11ed41c4d7511e84572bc
Berto Yáñez <berto@ber.to>
2016-02-24 13:00:16 +0100

    Initial

---
+++ in.js
@@ -0,0 +1,4 @@
+'use strict';
+
+var $ = require('jquery/src/core');
+require('jquery/src/core/init');
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    entry: './in.js',
+    output: {
+        path: __dirname,
+        filename: 'out.js'
+    }
+};
